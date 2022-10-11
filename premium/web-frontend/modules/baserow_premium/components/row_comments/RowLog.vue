c3cc0a092c872066cfc6c0c667b76a55ead24b73
Dalia-Salama-179 <daliasalama179@gmail.com>
2022-10-11 21:17:05 +0200

    Log style - render field key

--- premium/web-frontend/modules/baserow_premium/components/row_comments/RowLog.vue
+++ premium/web-frontend/modules/baserow_premium/components/row_comments/RowLog.vue
@@ -17,14 +17,57 @@
             </div>
         </div>
         <div class="row-comments__comment-text white-space-normal">
-            <div v-for="key in Object.keys(comment.params.original_row_values)">
-                <p class="field-title margin-bottom-0">{{ fieldName(key) }}</p>
-                <p class="margin-top-0 margin-bottom-1 color-primary-dark">
-                    <del>{{comment.params.original_row_values[key]}}</del>
-                    {{comment.params.new_row_values[key]}}
+            <div v-for="(key, idx) in Object.keys(comment.params.original_row_values)"
+                 :class="{'dotted-line-separator': idx < Object.keys(comment.params.original_row_values).length-1 }">
+                <p class="field-title color-black margin-top-1 d-flex margin-bottom-1 font-bold">
+                    {{ fieldName(key) }} :
                 </p>
+                <div class="margin-top-0 margin-bottom-1">
+                    <template v-if="Array.isArray(comment.params.original_row_values[key])">
+                        <template v-if="comment.params.original_row_values[key] &&
+                                        comment.params.original_row_values[key].length > 0">
+                            <template v-for="(el, index) in comment.params.original_row_values[key]">
+                                <div class="color-primary-dark"
+                                   v-for="objKey in Object.keys(el)">
+                                    <del class="old-val align-left forced-block"
+                                         v-if="objKey !== 'id'"> {{ el[objKey] }}
+                                    </del>
+                                    <p class="color-primary-dark margin-y0 align-right"
+                                       v-if="objKey !== 'id' && comment.params.new_row_values[key][index]">
+                                        {{comment.params.new_row_values[key][index][objKey]}}
+                                    </p>
+                                </div>
+                            </template>
+                        </template>
+
+                        <template v-else-if="comment.params.new_row_values[key] &&
+                                        comment.params.new_row_values[key].length > 0">
+                            <template v-for="(el, index) in comment.params.new_row_values[key]">
+                                <div class="color-primary-dark"
+                                   v-for="objKey in Object.keys(el)">
+                                    <del class="old-val align-left forced-block"
+                                         v-if="objKey !== 'id' && comment.params.original_row_values[key][index]">
+                                        {{comment.params.original_row_values[key][index][objKey]}}
+                                    </del>
+                                    <p class="color-primary-dark align-right" v-if="objKey !== 'id'">
+                                        {{ el[objKey] }}
+                                    </p>
+                                </div>
+                            </template>
+                        </template>
+                    </template>
+                    <div v-else class="color-primary-dark">
+                        <del class="old-val align-left forced-block">
+                            {{comment.params.original_row_values[key]}}
+                        </del>
+                        <p class="color-primary-dark margin-y0 align-right">
+                            {{comment.params.new_row_values[key]}}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
 </template>
 
@@ -42,8 +85,8 @@
       },
       fields: {
         type: Array,
-        required: true,
-      },
+        required: true
+      }
     },
     computed: {
       ...mapGetters({
@@ -72,5 +115,9 @@
 
     .field-title {
         font-size: 1rem;
+    }
+
+    .old-val {
+        color: #ed5a23;
     }
 </style>
