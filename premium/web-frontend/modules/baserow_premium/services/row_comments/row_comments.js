f38e2bf120d1da92ddbc0609061bb19caf819362
ahmed maghraby <38741761+ahmedmaghraby@users.noreply.github.com>
2023-04-03 17:20:18 +0300

    fetchActivityLog pagination

--- premium/web-frontend/modules/baserow_premium/services/row_comments/row_comments.js
+++ premium/web-frontend/modules/baserow_premium/services/row_comments/row_comments.js
@@ -1,6 +1,6 @@
 export default (client) => {
   return {
-    fetchAll(tableId, rowId, { offset = 0, limit = 50 }) {
+    fetchAll(tableId, rowId, { offset = 0, limit = 10 }) {
       return client.get(
         `/t2/row-comment/${tableId}/${rowId}/?offset=${offset}&limit=${limit}`
       )
@@ -8,7 +8,7 @@ export default (client) => {
     create(tableId, rowId, comment) {
       return client.post(`/t2/row-comment/${tableId}/${rowId}/`, { comment })
     },
-    fetchActivityLog(tableId, rowId, { offset = 0, limit = 50 }) {
+    fetchActivityLog(tableId, rowId, { offset = 0, limit = 10 }) {
       return client.get(
         `/t2/field-logs/?row=${rowId}&table=${tableId}&offset=${offset}&limit=${limit}`
         // `/t2/field-logs/?offset=${offset}&limit=${limit}`
